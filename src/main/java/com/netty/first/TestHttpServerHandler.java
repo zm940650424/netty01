999a0c5cd53aa8c92091a1f8d297f4620222a578
zhangmeng <zhangmeng@duiba.com.cn>
2019-09-01 09:39:24 +0800

    实现 SimpleChannelInboundHandler 处理请求的一下方法的重写，从而了解Handler的加载执行顺序。

--- src/main/java/com/netty/first/TestHttpServerHandler.java
+++ src/main/java/com/netty/first/TestHttpServerHandler.java
@@ -7,6 +7,8 @@ import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.handler.codec.http.*;
 import io.netty.util.CharsetUtil;
 
+import java.net.URI;
+
 /**
  * @author zhangmeng
  * @date 2019/8/31
@@ -17,7 +19,19 @@ public class TestHttpServerHandler extends SimpleChannelInboundHandler<HttpObjec
     @Override
     protected void channelRead0(ChannelHandlerContext ctx, HttpObject msg) throws Exception {
 
+        System.out.println(msg.getClass());
+        System.out.println(ctx.channel().remoteAddress());
+        Thread.sleep(8000);
         if(msg instanceof HttpRequest){
+
+            HttpRequest httpRequest = (HttpRequest) msg;
+
+            System.out.println("请求方法名:"+ httpRequest.method().name());
+            URI uri = new URI(httpRequest.uri());
+            if("/favicon.ico".equals(uri.getPath())){
+                System.out.println("请求 favicon");
+                return;
+            }
 
             ByteBuf content = Unpooled.copiedBuffer("Hello World", CharsetUtil.UTF_8);
             FullHttpResponse response = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK,content);
@@ -26,6 +40,42 @@ public class TestHttpServerHandler extends SimpleChannelInboundHandler<HttpObjec
             response.headers().set(HttpHeaderNames.CONTENT_LENGTH,content.readableBytes());
 
             ctx.writeAndFlush(response);
+            ctx.channel().close();
         }
+    }
+
+
+    /**
+     * 下面代码为了测试 运行顺序
+     */
+
+    @Override
+    public void channelRegistered(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("register");
+        super.channelRegistered(ctx);
+    }
+
+    @Override
+    public void channelUnregistered(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("unregister");
+        super.channelUnregistered(ctx);
+    }
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("active");
+        super.channelActive(ctx);
+    }
+
+    @Override
+    public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("inactive");
+        super.channelInactive(ctx);
+    }
+
+    @Override
+    public void handlerAdded(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("add");
+        super.handlerAdded(ctx);
     }
 }
